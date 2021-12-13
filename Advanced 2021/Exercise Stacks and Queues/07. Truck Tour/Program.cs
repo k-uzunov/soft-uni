10e8b78d3ce84c82b8e38b13667f413e4e55b505
k-uzunov <krasimiruzunov0@gmail.com>
2021-12-13 23:55:39 +0200

    Added Truck Tour - unfinished

---
+++ Advanced 2021/Exercise Stacks and Queues/07. Truck Tour/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _07._Truck_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPumps = int.Parse(Console.ReadLine());
+            Queue<int []> petrolPumps = new Queue<int[]> { };
+
+            for(int i = 0; i <numberOfPumps; i++)
+            {
+                int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                petrolPumps.Enqueue(input);
+            }
+
+            int startPump = 0;
+            bool circleNotComplete = true;
+
+            while (circleNotComplete)
+            {
+                int fuelInTank = 0;
+                foreach(int[] pump in petrolPumps)
+                {
+                    fuelInTank += pump[0];
+                    fuelInTank -= pump[1];
+                    if (fuelInTank <= 0)
+                    {
+                        startPump++;
+                        petrolPumps.Dequeue();
+                        petrolPumps.Enqueue(pump);
+                        break;
+                    }
+                }
+
+                if(fuelInTank >= 0)
+                {
+                    Console.WriteLine(startPump);
+                    circleNotComplete = false;
+                }
+            }
+
+
+
+        }
+    }
+}
+
+
+
+
+
