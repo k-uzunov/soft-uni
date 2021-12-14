282a6717a01968ba917a0e6486e9832a6ac7cc55
k-uzunov <krasimiruzunov0@gmail.com>
2021-12-14 22:33:24 +0200

    Added more exercises in Stacks and Queues

---
+++ Advanced 2021/Exercise Stacks and Queues/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Stack<char> brackets = new Stack<char>();
+
+            foreach(char bracket in input)
+            {
+                if(brackets.Count == 0)
+                {
+                    brackets.Push(bracket);
+                }
+                else
+                {
+                    if( bracket == '}' && brackets.Peek() == '{')
+                    {
+                        brackets.Pop();
+                    }else if(bracket == ']' && brackets.Peek() == '[')
+                    {
+                        brackets.Pop();
+                    }
+                    else if(bracket == ')' && brackets.Peek() == '(')
+                    {
+                        brackets.Pop();
+                    }
+                    else
+                    {
+                        brackets.Push(bracket);
+                    }
+
+                }
+            }
+
+            if (brackets.Count == 0) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
---
+++ Advanced 2021/Exercise Stacks and Queues/09. Simple Text Editor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder text = new StringBuilder();
+            int numOfCmd = int.Parse(Console.ReadLine());
+            Stack<string> previousVersion = new Stack<string>();
+            
+            for(int i = 0; i < numOfCmd; i++)
+            {
+                string input = Console.ReadLine();
+
+                if (input.StartsWith('1'))
+                {
+                    string commandArg = input.Substring(2);
+                    previousVersion.Push(text.ToString());
+                    text.Append(commandArg);
+                    
+                }
+
+                if (input.StartsWith('2'))
+                {
+                    string commandArg = input.Substring(2);
+                    previousVersion.Push(text.ToString());
+                    text.Remove(text.Length-int.Parse(commandArg), int.Parse(commandArg));
+                }
+
+                if (input.StartsWith('3'))
+                {
+                    string commandArg = input.Substring(2);
+                    string @string = text.ToString();
+                    Console.WriteLine(@string[int.Parse(commandArg)-1]);
+                }
+
+                if (input.StartsWith('4'))
+                {
+                    text.Clear();
+                    text.Append(previousVersion.Pop());
+                    Console.WriteLine("Undo");
+                }
+            }
+        }
+    }
+}
