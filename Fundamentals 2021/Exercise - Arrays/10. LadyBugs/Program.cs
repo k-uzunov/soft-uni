e3d0d331ce2baac37ba7a97036699d4b7558606d
k-uzunov <krasimiruzunov0@gmail.com>
2021-12-24 13:16:08 +0200

    Added Exercise LadyBugs, still have some bugs *no pun intened* to clean up

---
+++ Fundamentals 2021/Exercise - Arrays/10. LadyBugs/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fieldSize = int.Parse(Console.ReadLine());
+            int[] ladyBugs = new int[fieldSize];
+            ladyBugs.Initialize();
+
+            int[] indexes = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            foreach(int index in indexes)
+            {
+                if (index >= 0 && index < fieldSize)
+                {
+                    ladyBugs[index] = 1;
+                }
+            }
+
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (command[0] != "end")
+            {
+                int index = int.Parse(command[0]);
+                string direction = command[1];
+                int length = int.Parse(command [2]);
+                
+                if(index >= 0 && index < fieldSize && ladyBugs[index] == 1)
+                {
+                    if (direction == "right")
+                    {
+                        int newIndex = index + length;
+
+                        while(newIndex >= 0 && newIndex < fieldSize)
+                        {
+                            if(ladyBugs[newIndex] == 1)
+                            {
+                                //New index is ocupied. Ladybug flies length number indexes to the right
+                                newIndex += length;
+                            }
+                            else
+                            {
+                                ladyBugs[index] = 0;
+                                ladyBugs[newIndex] = 1;
+                                break;
+                            }
+                        }
+
+                        if (newIndex < 0 || newIndex >= fieldSize)
+                        {
+                            //Ladybug flies of the field
+                            ladyBugs[index] = 0;
+                        }
+                    }
+                    else if (direction == "left")
+                    {
+                        int newIndex = index - length;
+
+                        while (newIndex >= 0 && newIndex < fieldSize)
+                        {
+                            if (ladyBugs[newIndex] == 1)
+                            {
+                                //New index is ocupied. Ladybug flies length number indexes to the left.
+                                newIndex -= length;
+                            }
+                            else
+                            {
+                                ladyBugs[index] = 0;
+                                ladyBugs[newIndex] = 1;
+                                break;
+                            }
+                        }
+
+                        if (newIndex < 0 || newIndex >= fieldSize)
+                        {
+                            //Ladybug flies of the field
+                            ladyBugs[index] = 0;
+                        }
+                    }
+                }
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Console.WriteLine(string.Join(" ", ladyBugs));
+        }
+    }
+}
