cb71949ce6872fbe63f9784965c1702fb51511bb
k-uzunov <krasimiruzunov0@gmail.com>
2021-12-25 22:12:24 +0200

    Added more exercises

--- Fundamentals 2021/Arrays - More Exercise/05. Longest Increasing Subsequence/Program.cs
+++ Fundamentals 2021/Arrays - More Exercise/05. Longest Increasing Subsequence/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _05._Longest_Increasing_Subsequence
 {
@@ -6,7 +7,41 @@ namespace _05._Longest_Increasing_Subsequence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] len = new int[nums.Length];
+            int[] prev = new int[nums.Length];
+            for (int p = 0; p < nums.Length; p++)
+            {
+                len[p] = 1;
+                prev[p] = -1;
+
+                for (int left = 0; left < p; left++)
+                {
+                    if (nums[left] < nums[p])
+                    {
+                        //If left number is smaller than number on the position
+                        //Save it's position, and increase the legth
+                        len[p] = len[left] + 1;
+                        prev[p] = left;
+                    }
+                }
+            }
+            int[] subsequence = new int[len.Max()];
+            int index = subsequence.Length - 1;
+            for(int i = Array.IndexOf(len, len.Max()); i >=0;)
+            {
+                subsequence[index] = nums[i];
+                index--;
+                i = prev[i];
+            }
+
+
+            //Console.WriteLine(string.Join(" || ", nums));
+            //Console.WriteLine(string.Join(" || ", len));
+            //Console.WriteLine(string.Join(" || ", prev));
+
+            Console.WriteLine(string.Join(" ", subsequence));
+
         }
     }
 }
