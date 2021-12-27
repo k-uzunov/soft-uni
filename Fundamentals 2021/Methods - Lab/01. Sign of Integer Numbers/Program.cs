fb309c8cae9875e4a5eadc0848b457bd9d5f96a9
k-uzunov <krasimiruzunov0@gmail.com>
2021-12-27 14:50:33 +0200

    Added Methods - Lab

---
+++ Fundamentals 2021/Methods - Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sign(int.Parse(Console.ReadLine()));
+        }
+
+        static void Sign(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
---
+++ Fundamentals 2021/Methods - Lab/02. Grades/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _02._Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            Grades(grade);
+        }
+        static void Grades(double grade)
+        {
+            if (grade >= 2.00 && grade <= 2.99 )
+            {
+                //Fail
+                Console.WriteLine("Fail");
+            }
+            else if (grade >= 3.00 && grade <= 3.49)
+            {
+                //Poor
+                Console.WriteLine("Poor");
+            }
+            else if (grade >= 3.50 && grade <= 4.49)
+            {
+                //Good
+                Console.WriteLine("Good");
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                //Very good
+                Console.WriteLine("Very good");
+            }
+            else if (grade >= 5.50 && grade <= 6.00)
+            {
+                ///Excellent
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
