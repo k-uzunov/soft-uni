72968212b5a796b3762eddf9689768553f76c4ec
k-uzunov <krasimiruzunov0@gmail.com>
2021-12-27 16:45:51 +0200

    Added Methods - Lab

---
+++ Fundamentals 2021/Methods - Lab/04. Printing Triangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i < n; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            for (int i = n; i > 0; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
