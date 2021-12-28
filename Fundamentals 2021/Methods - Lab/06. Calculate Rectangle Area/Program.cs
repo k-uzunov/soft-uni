5f4a5eb51ae77b2b513403c41fd0ef658196ef3d
k-uzunov <krasimiruzunov0@gmail.com>
2021-12-28 17:52:08 +0200

    Added Methods - Lab

---
+++ Fundamentals 2021/Methods - Lab/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double area = RectangleArea(height, width);
+
+            Console.WriteLine(area);
+        }
+
+        static double RectangleArea(double height, double width)
+        {
+            return height * width;
+        }
+    }
+}
---
+++ Fundamentals 2021/Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(text, count));
+        }
+
+        static string RepeatString(string text, int count)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                result += text;
+            }
+
+            return result;
+        }
+    }
+}
