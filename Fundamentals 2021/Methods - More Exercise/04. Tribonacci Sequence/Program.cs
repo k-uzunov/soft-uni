7dd97fd7b18f371737e4f41044c56ab1fc2dfc2a
k-uzunov <krasimiruzunov0@gmail.com>
2022-01-09 22:38:54 +0200

    Methods - More Exercises

--- Fundamentals 2021/Methods - More Exercise/04. Tribonacci Sequence/Program.cs
+++ Fundamentals 2021/Methods - More Exercise/04. Tribonacci Sequence/Program.cs
@@ -6,7 +6,33 @@ namespace _04._Tribonacci_Sequence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num = int.Parse(Console.ReadLine());
+
+            int[] tribunacci = GetTribunacci(num);
+
+            Console.WriteLine(string.Join(" ", tribunacci));
         }
+
+        private static int[] GetTribunacci(int num)
+        {
+            int[] result = new int[num];
+            result[0] = 1;
+            for (int i = 1; i < num; i++)
+            {
+                if (i < 3)
+                {
+                    result[i] = i;
+                }
+                else
+                {
+                    result[i] = result[i - 1] + result[i - 2] + result[i - 3];
+                }
+                
+            }
+
+            return result;
+        }
+
+        
     }
 }
---
+++ Fundamentals 2021/Methods - More Exercise/05. Multiplication Sign/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _05._Multiplication_Sign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            GetSign(num1, num2, num3);
+        }
+
+        private static void GetSign(int num1, int num2, int num3)
+        {
+            bool isNegative = false;
+
+            int[] numbers = new int[3]
+            {
+                num1, num2, num3
+            };
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    Console.WriteLine("zero");
+                    return;
+                }
+                else if (numbers[i] < 0)
+                {
+                    if (isNegative)
+                    {
+                        isNegative = false;
+                    }
+                    else
+                    {
+                        isNegative = true;
+                    }
+                }
+            }
+
+            if (isNegative)
+            {
+                Console.WriteLine("negative");
+            }
+            else
+            {
+                Console.WriteLine("positive");
+            }
+        }
+    }
+}
