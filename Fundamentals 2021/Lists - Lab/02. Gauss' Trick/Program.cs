b2a9e2777bcfa52beab09225e4f3b9ef5a5e0101
k-uzunov <krasimiruzunov0@gmail.com>
2022-01-11 20:07:21 +0200

    Added Lists - Lab

---
+++ Fundamentals 2021/Lists - Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //first + last, first + 1 + last - 1, first + 2 + last - 2, … first + n, last - n.
+
+            List<double> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToList();
+
+            Console.WriteLine(string.Join(" ", Gauss(numbers)));
+        }
+
+        static List<double> Gauss(List<double> numbers)
+        {
+            List<double> gaussTrick = new List<double> { };
+            int lastIndex = numbers.Count - 1;
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                gaussTrick.Add(numbers[i] + numbers[lastIndex - i]);
+            }
+            if (numbers.Count % 2 == 1)
+            {
+                gaussTrick.Add(numbers[numbers.Count / 2]);
+            }
+            return gaussTrick;
+        }
+    }
+}
---
+++ Fundamentals 2021/Lists - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondList = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> merged = Merge(firstList, secondList);
+
+            Console.WriteLine(string.Join(" ", merged));
+        }
+
+        private static List<int> Merge(List<int> listA, List<int> listB)
+        {
+            List<int> result = new List<int> { };
+
+            while (listA.Count > 0 && listB.Count > 0)
+            {
+                result.Add(listA[0]);
+                listA.RemoveAt(0);
+                result.Add(listB[0]);
+                listB.RemoveAt(0);
+            }
+
+            if (listB.Count > 0)
+            {
+                for (int i = 0; i < listB.Count; i++)
+                {
+                    result.Add(listB[i]);
+                }
+            }
+            else if (listA.Count > 0)
+            {
+                for (int i = 0; i < listA.Count; i++)
+                {
+                    result.Add(listA[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
