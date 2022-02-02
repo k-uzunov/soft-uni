56656eb1c686d267f1148cda407ae5dcad1d464c
k-uzunov <krasimiruzunov0@gmail.com>
2022-02-02 17:43:16 +0200

    DOM Introduction - Exercise

--- JS Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
+++ JS Advanced/DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,25 @@
 function generateReport() {
-    //TODO
+    let tableHead = Array.from(document.querySelectorAll("th input"));
+    let tableRows =  Array.from(document.querySelectorAll("tbody tr"));
+    let table = [];
+    let output = [];
+
+    for (const row of tableRows) {
+        let currentRow = Array.from(row.children).map(tr => tr.textContent)
+        table.push(currentRow);
+    }
+
+    for(let row = 0; row < table.length; row++){
+        let resultObj = {}
+        for(let col = 0; col < table[row].length; col++){
+            
+            if (tableHead[col].checked) {
+                resultObj[tableHead[col].name] = table[row][col];
+            }
+        }
+        output.push(resultObj);
+    }
+
+    document.getElementById("output").textContent = JSON.stringify(output);
+
 }
--- JS Advanced/DOM Introduction - Exercise/09. Number Convertor/solution.js
+++ JS Advanced/DOM Introduction - Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    let binaryOpt = document.createElement("option");
+    binaryOpt.value = "binary";
+    binaryOpt.textContent = "Binary";
+    let hexOpt = document.createElement("option");
+    hexOpt.value = "hexadecimal";
+    hexOpt.textContent = "Hexadecimal";
 
-    //TODO...
+    let selectElement = document.getElementById("selectMenuTo");
+    selectElement.appendChild(binaryOpt);
+    selectElement.appendChild(hexOpt);
+
+    document.querySelector("button").addEventListener('click', onClick);
+
+    function onClick(){
+        let number = Number(document.getElementById("input").value);
+        console.log(typeof(number));
+        let result;
+        let selectTo = document.getElementById('selectMenuTo').value;
+        if (selectTo == "binary") {
+            result = number.toString(2).toUpperCase();
+        }else{
+            result = number.toString(16).toUpperCase();
+        }
+        document.getElementById("result").value = result;
+    }
+
 }
