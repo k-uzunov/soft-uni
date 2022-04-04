1924d3385c81e30930a8e59acd037334f005769a
k-uzunov <krasimiruzunov0@gmail.com>
2022-04-04 20:20:34 +0300

    Commited old changes

--- JS Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/04.Locked-Profile/app.js
+++ JS Applications/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/04.Locked-Profile/app.js
@@ -1,3 +1,7 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function lockedProfile() {
+    const url = 'http://localhost:3030/jsonstore/advanced/profiles'
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
 }
